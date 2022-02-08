74dc160ed597659a11fec3bf8cd860486daf6f45
jerrywise97 <54330961+jerrywise97@users.noreply.github.com>
2022-02-09 00:02:53 +0100

    "second push"

--- src/main/java/com/employee/employee/data/repository/EmployeeRepository.java
+++ src/main/java/com/employee/employee/data/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.employee.employee.data.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
---
+++ src/test/java/com/employee/employee/data/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.employee.employee.data.repository;
+
+import com.employee.employee.data.models.Employee;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
+@Sql(scripts = {"/db/insert.sql"})
+class EmployeeRepositoryTest {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    @DisplayName("save a new employee to the datebase")
+    public void saveEmployeeInTheDataBase(){
+        //given i create a new employee
+        Employee employee = new Employee();
+        employee.setName("jerry");
+        employee.setEmail("@gmail.com");
+        employee.setMobileNumber("08032912334");
+        employee.setPassword("jerry289");
+        employee.setUserName("simplejey");
+
+        assertThat(employee.getId()).isNull();
+        //when i save employee
+        employeeRepository.save(employee);
+
+        //then i check if the employee is save
+        log.info("employee saved :: {}", employee);
+
+        assertThat(employee.getId()).isNotNull();
+        assertThat(employee.getName()).isEqualTo("jerry");
+        assertThat(employee.getEmail()).isEqualTo("@gmail.com");
+        assertThat(employee.getMobileNumber()).isEqualTo("08032912334");
+        assertThat(employee.getUserName()).isEqualTo("simplejey");
+    }
+
+    @Test
+    @DisplayName("find an existing employee from the database")
+    public void findExistingEmployeeFromDatabaseTest(){
+        Employee employee = employeeRepository.findById(12L).orElse(null);
+
+        assert employee != null;
+        assertThat(employee.getId()).isEqualTo(12);
+        assertThat(employee.getName()).isEqualTo("giidy");
+        assertThat(employee.getEmail()).isEqualTo("chris1@gmail.com");
+        assertThat(employee.getMobileNumber()).isEqualTo("08037227");
+        assertThat(employee.getUserName()).isEqualTo("luxury1");
+
+        log.info("employee saved :: {}", employee);
+    }
+
+    @Test
+    @DisplayName("find all employee in the databse")
+    public void findAllEmployeeTest(){
+        List<Employee> employeeList = employeeRepository.findAll();
+        assertThat(employeeList.size()).isEqualTo(4);
+        assertThat(employeeList).isNotNull();
+    }
+}
